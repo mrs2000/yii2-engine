588e40c42d1d727c69bcd51407e2f00a14e259bc
Melnikov Ruslan <melnikov@novator.angara.ru>
2014-12-15 11:11:11 +0800

    15.12.2014

--- Asset.php
+++ Asset.php
@@ -20,5 +20,6 @@ class Asset extends AssetBundle
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapThemeAsset'
     ];
 }
