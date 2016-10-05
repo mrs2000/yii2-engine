b31b749e47255e696ed89011f9986aabd275561e
Melnikov Ruslan <melnikov@novator.angara.ru>
2016-10-05 16:57:24 +0800

    Compress assets

--- AssetManager.php
+++ AssetManager.php
@@ -6,7 +6,7 @@ class AssetManager extends \yii\web\AssetManager
     public function getAssetUrl($bundle, $asset)
     {
         $url = parent::getAssetUrl($bundle, $asset);
-        if (!(strpos($url, '//') === 0) && !(strpos($url, 'http') === 0) && !(strpos($url, '/assets/') === 0)) {
+        if (!(strpos($url, '//') === 0) && !(strpos($url, 'http') === 0) && !(strpos($url, '/assets/') === 0 && \Yii::$app->controller->module->id == 'admin')) {
             $url = '/' . filemtime('.' . $url) . $url;
         }
 
