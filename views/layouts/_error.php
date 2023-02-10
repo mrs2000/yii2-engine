1d0d91d405741b166742cd4ab1bd1d80f75347eb
Melnikov Ruslan <melnikov@novator.angara.ru>
2023-02-10 12:24:01 +0800

    1.3.6

--- views/layouts/_error.php
+++ views/layouts/_error.php
@@ -1,4 +1,5 @@
 <?php
+
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
@@ -11,6 +12,4 @@ $this->title = $name;
 <div class="site-error">
     <h1><?=Html::encode($this->title)?></h1>
     <div class="alert alert-danger"><?=nl2br(Html::encode($message))?></div>
-    <p>The above error occurred while the Web server was processing your request.</p>
-    <p>Please contact us if you think this is a server error. Thank you.</p>
 </div>
